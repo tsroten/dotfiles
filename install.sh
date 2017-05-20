bab1130f4023567aa75b435b5dede3b9c56611f7
Thomas Roten <tsr@lavabit.com>
2017-05-19 20:57:26 -0500

    Add install script.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")" || exit;
+
+git pull origin master;
+
+doSync() {
+  rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude "install.sh" \
+    --exclude "README.md" \
+    --exclude "LICENSE.txt" \
+    -avh --no-perms . ~;
+  . ~/.bash_profile;
+}
+
+if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
+  doSync;
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+  echo "";
+  if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+    doSync;
+  fi;
+fi;
+unset doSync;
