fd7ba09b3253638f4bc32c8477834f3df6b62131
Thomas Roten <tsr@thomasroten.com>
2026-06-03 19:55:37 -0400

    add start-day script

---
+++ README.md
@@ -0,0 +1,3 @@
+brew install coreutils neovim python@3 rsync terraform tfenv tmux urlview
+
+brew --cask install alacritty claude claude-code gcloud-cli google-cloud-sdk zed
--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 #
 # Install the dotfiles.
 
-cd "$(dirname "${BASH_SOURCE}")" || exit
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 git pull origin master
 
 doSync() {
-  /opt/homebrew/bin/rsync --exclude ".git/" \
+  rsync --exclude ".git/" \
     --exclude ".DS_Store" \
     --exclude "install.sh" \
     --exclude "README.md" \
     --exclude "LICENSE.txt" \
     --exclude "dotfiles.code-workspace" \
-    -avh --no-perms . ~;
+    -avh --no-perms . ~
 }
 
-if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
+if [ "${1:-}" = "--force" ] || [ "${1:-}" = "-f" ]; then
   doSync
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+  read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) "
   echo ""
   if [[ "$REPLY" =~ ^[Yy]$ ]]; then
     doSync
-  fi;
-fi;
-unset doSync
+  fi
+fi
